27e5c0b91bb706519de0b0270549f649588c854c
Masato Naka <masatonaka1989@gmail.com>
2022-11-18 07:17:49 +0900

    remove unused test

--- simpledb/app/src/test/java/simpledb/AppTest.java
+++ simpledb/app/src/test/java/simpledb/AppTest.java
@@ -3,12 +3,5 @@
  */
 package simpledb;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-
 class AppTest {
-    @Test void appHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
-    }
 }
