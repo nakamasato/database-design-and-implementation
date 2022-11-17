20db674df049162c4dce88385b70240d30685883
Masato Naka <masatonaka1989@gmail.com>
2022-11-18 06:47:41 +0900

    2.1. Write/read contents to/from a file

--- simpledb/app/src/main/java/simpledb/App.java
+++ simpledb/app/src/main/java/simpledb/App.java
@@ -3,12 +3,17 @@
  */
 package simpledb;
 
+import java.io.File;
+
+import simpledb.file.FileMgr;
+
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        File dbDirectory = new File("datadir");
+        FileMgr fm = new FileMgr(dbDirectory, 400);
+        String filename = "test.txt";
+        fm.write(filename);
+        fm.read(filename);
     }
 }
